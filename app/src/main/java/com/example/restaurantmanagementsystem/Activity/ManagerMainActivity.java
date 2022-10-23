8105374b79e0a0558165f4e3f922f041994ef753
ant12 <516622533@qq.com>
2022-10-23 11:17:35 +0800

    initialization

---
+++ app/src/main/java/com/example/restaurantmanagementsystem/Activity/ManagerMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.restaurantmanagementsystem.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restaurantmanagementsystem.R;
+
+public class ManagerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manager_main_layout);
+    }
+}
