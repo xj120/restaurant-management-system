124c4e8e898498423f41e8476b6b0a8fee208cd4
ant12 <516622533@qq.com>
2022-12-04 11:49:11 +0800

    update

--- app/src/main/java/com/example/restaurantmanagementsystem/Bill/Bill.java
+++ app/src/main/java/com/example/restaurantmanagementsystem/Bill/Bill.java
@@ -3,5 +3,36 @@ package com.example.restaurantmanagementsystem.Bill;
 public class Bill {
     private int bill_id;
     private int customer_id;
+
+    public Bill(int bill_id, int customer_id, int price) {
+        this.bill_id = bill_id;
+        this.customer_id = customer_id;
+        this.price = price;
+    }
+
+    public int getBill_id() {
+        return bill_id;
+    }
+
+    public void setBill_id(int bill_id) {
+        this.bill_id = bill_id;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private int price;
 }
