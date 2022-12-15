ab9089e8f158875301a39d5b035d7b9fc57c937d
clteafro <446365043@qq.com>
2022-12-15 16:42:55 +0800

    菜单 账目

---
+++ app/src/main/java/com/example/restaurantmanagementsystem/Activity/AccountsManagement.java
@@ -0,0 +1,16 @@
+package com.example.restaurantmanagementsystem.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restaurantmanagementsystem.R;
+
+public class AccountsManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accounts_management);
+    }
+}
