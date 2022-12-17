d382b98947e1f336ca1f5ee51f54083a6257f2a3
ant12 <516622533@qq.com>
2022-12-18 01:49:36 +0800

    实现客户端

--- app/src/main/java/com/example/restaurantmanagementsystem/Activity/CustomerOrderActivity.java
+++ app/src/main/java/com/example/restaurantmanagementsystem/Activity/CustomerOrderActivity.java
@@ -2,14 +2,33 @@ package com.example.restaurantmanagementsystem.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.restaurantmanagementsystem.Adapter.OrderAdapter;
+import com.example.restaurantmanagementsystem.DatabaseHelper.DatabaseHelper;
+import com.example.restaurantmanagementsystem.Order.Order;
 import com.example.restaurantmanagementsystem.R;
 
+import java.util.List;
+
 public class CustomerOrderActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
+    private List<Order> orderList;
+    private Intent gIntent;
+    private int customerId;
+    private OrderAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_manage_order_layout);
+
+        dbHelper = new DatabaseHelper(this, "Restaurant.db", null, 2);
+
+        gIntent = getIntent();
+        customerId = gIntent.getIntExtra("customer_id", 0);
     }
 }
